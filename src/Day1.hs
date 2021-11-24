996d8aa3210ff35faf2a3f24a71934fd5fc7d12a
Theo Garefalakis <tgaref@gmail.com>
2021-11-24 12:49:18 +0200

    Added sample day in src.

---
+++ src/Day1.hs
@@ -0,0 +1,18 @@
+{- |
+Copyright: (c) 2021 Theo Garefalakis
+SPDX-License-Identifier: MIT
+Maintainer: Theo Garefalakis <tgaref@gmail.com>
+
+AoC 2021 solutions using Haskell
+-}
+
+module Day1
+       ( day1a
+       , day1b
+       ) where
+
+
+
+day1a = undefined
+
+day1b = undefined
