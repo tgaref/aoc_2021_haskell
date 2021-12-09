92ec3bf175ede8ee34be860a9f63fc929971922b
Theo Garefalakis <tgaref@gmail.com>
2021-12-09 12:22:14 +0200

    Complete day 9.

---
+++ app/Day9.hs
@@ -0,0 +1,58 @@
+module Main (main) where
+
+import           Lib (readDigit, fixed, Grid, Coords (..), cell, gridSize, imap2)
+import qualified Data.Text as T
+import           Data.Vector (Vector)
+import qualified Data.Vector as V
+import qualified Data.Attoparsec.Text as A 
+import qualified Data.Set as S
+
+-- Read input
+
+readInput :: FilePath -> IO (Grid Int)
+readInput file = V.fromList . fmap parseLine . T.lines <$> readFileText file 
+
+parseLine :: Text -> Vector Int
+parseLine t = case A.parseOnly (A.many1' A.digit) t of
+  Left _  -> error "Failed to parse input..."
+  Right a -> V.fromList $ fmap readDigit a
+
+-- Solution
+
+neighbours :: Grid Int -> Coords -> [Coords]  
+neighbours grid (C i j) =
+  filter (\(C a b) -> (a >= 0) && (a <= x) && (b >= 0) && (b <= y)) [ C i (j-1), C i (j+1), C (i-1) j, C (i+1) j]      
+  where
+    (x,y) = gridSize grid
+
+isLow :: Grid Int -> Coords -> Bool
+isLow grid p = all (\q -> cell grid q > h) nei
+  where
+    nei = neighbours grid p
+    h = cell grid p
+
+higherNeighbours :: Grid Int -> Coords -> [Coords]
+higherNeighbours grid p = filter (\q -> let h' = cell grid q in h' > h && h' < 9) $ neighbours grid p
+  where
+    h = cell grid p
+
+expand :: Grid Int -> Set Coords -> Set Coords
+expand grid set = S.foldl' (\acc p -> S.union acc (S.fromList (higherNeighbours grid p))) set set 
+
+basin :: Grid Int -> Coords -> Set Coords
+basin grid p = fixed (expand grid) (S.singleton p)
+
+day9a :: Grid Int -> Int
+day9a grid = sum $ sum <$> imap2 (\i j _ -> if isLow grid (C i j) then 1 + cell grid (C i j) else 0) grid  
+
+day9b :: Grid Int -> Int
+day9b grid = product . take 3 . sortOn Down . fmap (S.size . basin grid) $ lowPoints
+  where
+    lowPoints = V.ifoldl' (\acc j row ->
+                             V.ifoldl' (\acc' i _ -> if isLow grid (C i j) then C i j : acc' else acc') acc row) [] grid 
+    
+main :: IO ()
+main = do 
+  input <- readInput "inputs/9.input"
+  print $  day9a input
+  print $  day9b input
--- src/Lib.hs
+++ src/Lib.hs
@@ -6,23 +6,56 @@ Maintainer: Theo Garefalakis <tgaref@gmail.com>
 AoC 2021 solutions using Haskell
 -}
 
-module Lib (readInt, simulate, iterfix) where
+module Lib (readInt, readDigit
+           , simulate, fixed
+           , Grid, Coords (..), cell, gridSize, map2, imap2) where
 
 import qualified Data.Text.Read as T
+import           Data.Vector (Vector, (!))
+import qualified Data.Vector as V 
+
+type Grid a = Vector (Vector a)
+data Coords =  C {-# UNPACK #-} !Int {-# UNPACK #-} !Int
+  deriving stock (Eq, Ord, Show)
+
+cell :: Grid a -> Coords -> a
+cell grid (C i j) = (grid ! j) ! i
+
+gridSize :: Grid a -> (Int, Int)
+gridSize grid = (V.length (grid ! 0) - 1, V.length grid - 1)
+
+map2 :: (a -> b) -> Grid a -> Grid b
+map2 f = V.map (V.map f)
+
+imap2 :: (Int -> Int -> a -> b) -> Grid a -> Grid b 
+imap2 f = V.imap (\j row -> V.imap (\i v -> f i j v) row)
   
 readInt :: Text -> Int
 readInt t = case T.decimal t of
   Left _       -> error "Failed to read int"
   Right (i, _) -> i 
 
+readDigit :: Char -> Int
+readDigit '0' = 0
+readDigit '1' = 1 
+readDigit '2' = 2
+readDigit '3' = 3
+readDigit '4' = 4
+readDigit '5' = 5 
+readDigit '6' = 6
+readDigit '7' = 7 
+readDigit '8' = 8
+readDigit '9' = 9
+readDigit _   = error "Failed to read digit..."  
+
 simulate :: Int -> (a -> a) -> a -> a
 simulate !n f !a
  | n <= 0    = a
  | otherwise = simulate (n-1) f (f a) 
 
-iterfix :: Eq a => (a -> a) -> a -> a
-iterfix f x = if x == x'
+fixed :: Eq a => (a -> a) -> a -> a
+fixed f x = if x == x'
               then x
-              else iterfix f x'
+              else fixed f x'
   where
     x' = f x
